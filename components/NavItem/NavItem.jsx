3c26bb11255d310aa7e4d1a7a47e87f59591adb2
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-02-02 13:18:35 +0800

    components: added subnav styles

--- components/NavItem/NavItem.jsx
+++ components/NavItem/NavItem.jsx
@@ -5,10 +5,37 @@ const NavItem = ({ nav, active }) => {
 
   return (
     <li className={styles.navItem}>
-      <Link href={url ? url : "/"}>
+      <Link href={url != null ? url : "/"}>
         <a className="block px-5 py-5"> {label} </a>
       </Link>
-      {/* {subNav && <div>subnav here</div>} */}
+      {subNav && (
+        <div
+          className={styles.subNav}
+          style={{
+            transformOrigin: direction === "left" ? "top left" : "top right",
+            right: direction === "left" ? "auto" : "0",
+          }}
+        >
+          <div className="overflow-hidden bg-white p-2 shadow-md">
+            {subNav.map((subNavItem, index) => {
+              return (
+                <div className="p-4 transition-all hover:bg-adaptive-white-2">
+                  <Link href={subNavItem.url != null ? subNavItem.url : "/"}>
+                    <a>
+                      <p className="mb-1 font-semibold text-primary-color">
+                        {subNavItem.label}
+                      </p>
+                      <small className="text-primary-color opacity-80">
+                        {subNavItem.subLabel}
+                      </small>
+                    </a>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </li>
   );
 };

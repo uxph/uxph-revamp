2df7c221b3f432f368f67389afc20609e7362e90
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-04-10 19:25:50 +0800

    added member list and modal

---
+++ components/MemberModal/MemberModal.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+const MemberModal = ({ member }) => {
+  const { image, name, team } = member;
+
+  if (member) {
+    return (
+      <div className="absolute top-1/4 left-1/4 z-10 flex w-1/2 rounded-md bg-white p-12 shadow-sm">
+        <div className="flex items-start gap-4">
+          <div className="relative w-64" style={{ height: "110px" }}>
+            <Image
+              className="rounded-full"
+              src={image.files[0].file.url}
+              objectFit="cover"
+              alt={name.title[0].plain_text}
+              layout="fill"
+            />
+          </div>
+          <div>
+            <h3 className="text-primary-color">{name.title[0].plain_text}</h3>
+            <p className="text-brand-color">{team.multi_select[0].name}</p>
+            <h5>Bio</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              est, optio earum praesentium cum numquam laborum dolores officia
+              illum quam asperiores quas molestias voluptates? Earum labore
+              numquam esse necessitatibus consequuntur?
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return <></>;
+};
+
+export default MemberModal;
--- pages/team.js
+++ pages/team.js
@@ -1,11 +1,40 @@
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import notion from "../utils/notion";
+import Hero from "./../components/Hero/Hero";
+import MemberCard from "./../components/MemberCard/MemberCard";
+import { useState } from "react";
+import MemberModal from "./../components/MemberModal/MemberModal";
 
-const Team = () => {
+const Team = ({ advisors, volunteers, leads }) => {
+  const [modal, setModal] = useState(false);
+
+  const openModal = (member) => {
+    setModal(member);
+    // document.body.style.overflow = "hidden";
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
   return (
     <Layout>
+      <MemberModal member={modal} closeModal={closeModal} />
       <SEO title="Organizing Members" />
+      <Hero headingText="Organizing Members" />
+      <section className="py-12">
+        <h2 className="text-center">Team Leads</h2>
+        <div className="grid grid-cols-3 items-stretch justify-center px-80">
+          {leads.map((member) => {
+            return (
+              <>
+                <MemberCard member={member} openModal={openModal} />
+              </>
+            );
+          })}
+        </div>
+      </section>
     </Layout>
   );
 };
@@ -27,7 +56,6 @@ export async function getStaticProps() {
     getTeamMembersData(process.env.NOTION_VOLUNTEERS_DATABASE),
     getTeamMembersData(process.env.NOTION_LEADS_DATABASE),
   ]);
-
   return {
     props: { advisors, volunteers, leads },
     revalidate: 300,

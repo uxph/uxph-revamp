1c9226b329fde94ec8180a7f846b0236bbc66819
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-05-30 21:06:22 +0800

    clean team list code

--- pages/team.js
+++ pages/team.js
@@ -6,7 +6,7 @@ import MemberCard from "./../components/MemberCard/MemberCard";
 import { useState } from "react";
 import MemberModal from "./../components/MemberModal/MemberModal";
 import Modal from "../components/Modal/Modal";
-
+import TeamList from "../components/TeamList/TeamList";
 const Team = ({ advisors, volunteers, leads }) => {
   const [modal, setModal] = useState(false);
 
@@ -27,42 +27,8 @@ const Team = ({ advisors, volunteers, leads }) => {
       </Modal>
       <SEO title="Organizing Members" />
       <Hero headingText="Organizing Members" />
-      <section className="py-12">
-        <div className="container mx-auto">
-          <h2 className="text-center">Team Leads</h2>
-          <div className="grid grid-flow-row grid-cols-1 items-stretch justify-center md:grid-cols-2 lg:grid-cols-3">
-            {leads.map((member, index) => {
-              return (
-                <div className="mx-auto">
-                  <MemberCard
-                    key={index}
-                    member={member}
-                    openModal={openModal}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </section>
-      <section className="py-12">
-        <div className="container mx-auto">
-          <h2 className="text-center">Volunteers</h2>
-          <div className="grid grid-cols-1 items-stretch justify-center gap-10  md:grid-cols-2 lg:grid-cols-3">
-            {volunteers.map((member, index) => {
-              return (
-                <>
-                  <MemberCard
-                    key={index}
-                    member={member}
-                    openModal={openModal}
-                  />
-                </>
-              );
-            })}
-          </div>
-        </div>
-      </section>
+      <TeamList members={leads} openModal={openModal} />
+      <TeamList members={volunteers} openModal={openModal} />
     </Layout>
   );
 };

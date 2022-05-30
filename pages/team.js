c73ceb6317f8d601d523e8cfc5950464efc30d2a
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-05-30 21:01:43 +0800

    fix grid layout

--- pages/team.js
+++ pages/team.js
@@ -5,10 +5,9 @@ import Hero from "./../components/Hero/Hero";
 import MemberCard from "./../components/MemberCard/MemberCard";
 import { useState } from "react";
 import MemberModal from "./../components/MemberModal/MemberModal";
-import Overlay from "./../components/Overlay/Overlay";
+import Modal from "../components/Modal/Modal";
 
 const Team = ({ advisors, volunteers, leads }) => {
-  // TODO: refactor modal implementation
   const [modal, setModal] = useState(false);
 
   const openModal = (member) => {
@@ -23,32 +22,45 @@ const Team = ({ advisors, volunteers, leads }) => {
 
   return (
     <Layout>
-      <Overlay active={modal} close={closeModal} />
-      <MemberModal member={modal} closeModal={closeModal} />
+      <Modal active={modal} close={closeModal}>
+        <MemberModal member={modal} />
+      </Modal>
       <SEO title="Organizing Members" />
       <Hero headingText="Organizing Members" />
       <section className="py-12">
-        <h2 className="text-center">Team Leads</h2>
-        <div className="grid grid-cols-3 items-stretch justify-center gap-10 px-48">
-          {leads.map((member) => {
-            return (
-              <>
-                <MemberCard member={member} openModal={openModal} />
-              </>
-            );
-          })}
+        <div className="container mx-auto">
+          <h2 className="text-center">Team Leads</h2>
+          <div className="grid grid-flow-row grid-cols-1 items-stretch justify-center md:grid-cols-2 lg:grid-cols-3">
+            {leads.map((member, index) => {
+              return (
+                <div className="mx-auto">
+                  <MemberCard
+                    key={index}
+                    member={member}
+                    openModal={openModal}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
       <section className="py-12">
-        <h2 className="text-center">Volunteers</h2>
-        <div className="grid grid-cols-3 items-stretch justify-center gap-10 px-48">
-          {volunteers.map((member) => {
-            return (
-              <>
-                <MemberCard member={member} openModal={openModal} />
-              </>
-            );
-          })}
+        <div className="container mx-auto">
+          <h2 className="text-center">Volunteers</h2>
+          <div className="grid grid-cols-1 items-stretch justify-center gap-10  md:grid-cols-2 lg:grid-cols-3">
+            {volunteers.map((member, index) => {
+              return (
+                <>
+                  <MemberCard
+                    key={index}
+                    member={member}
+                    openModal={openModal}
+                  />
+                </>
+              );
+            })}
+          </div>
         </div>
       </section>
     </Layout>

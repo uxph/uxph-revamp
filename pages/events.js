4c03517f18afffedf5e5d9d8f1817cfc1dc4850e
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-05-30 21:58:04 +0800

    refactor hero implementation

--- pages/events.js
+++ pages/events.js
@@ -6,36 +6,17 @@ import Hero from "../components/Hero/Hero";
 import Button from "./../components/Button/Button";
 import EventItem from "./../components/EventItem/EventItem";
 import { eventFilter } from "../utils/notion-filters";
+import Image from "next/image";
+
 const Events = ({ events }) => {
   return (
     <Layout>
       <SEO title="Events" />
-      <Hero
-        imageUrl="/images/home/community-partner.png"
-        headingText="UX Philippines"
-        content={
-          <>
-            <p className="mb-5">
-              <b>
-                Welcome to the Philippines' largest community of individuals
-                passionate about the realm of UX (User Experience), and all
-                things related.
-              </b>
-              Our community has over 4,000 local and international members from
-              a wide range of industries related to technology and even
-              traditional businesses.
-            </p>
-            <div className="flex flex-col lg:flex-row">
-              <Button variant="white" className="mb-5 lg:mb-0 lg:mr-5">
-                Learn More
-              </Button>
-              <Button variant="white-alternative">
-                Join our Facebook group
-              </Button>
-            </div>
-          </>
-        }
-      />
+      <Hero>
+        <div className="w-full">
+          <h1 className="text-center">Events</h1>
+        </div>
+      </Hero>
       <section className="py-48">
         <div className="grid grid-cols-3 items-stretch justify-center gap-8 px-32">
           {events.map((event, index) => {
